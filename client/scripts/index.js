b6457442700cc775fd0cccba771daa8cfb2be9c3
Jochen <jochen.panjaer@ziango.com>
2019-03-08 13:30:31 +0100

    hook up react

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,3 +1,11 @@
 import '../styles/index.scss';
 
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/App';
+
+const reactContainer = document.querySelector('#reactContainer');
+ReactDom.render(<App />, reactContainer);
+
 console.log('scripts booted');
